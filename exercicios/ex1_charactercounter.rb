87cf072e422237a0094f82ad33b825ce1cbde8d0
Jessé Cirilo Soares <jessecsoares@gmail.com>
2013-08-19 21:22:45 -0300

    Enunciado dos exercicios 1 e 2

---
+++ exercicios/ex1_charactercounter.rb
@@ -0,0 +1,7 @@
+=begin
+	
+1) Criar um método que receba 2 parâmetros, 1 texto e 1 caracter e retorne a quantidade de vezes que o caracter é encontrado no texto
+
+2) Criar um método que receba 2 parâmetros, 1 texto e 1 array de caracteres e retorne a quantidade de vezes que os caracteres são encontrados no texto
+
+end
---
+++ exercicios/ex2_jsonparser.rb
@@ -0,0 +1,23 @@
+=begin
+	
+Criar um método que receba um objeto em ruby e retorne seu valor em 1 string no formato JSON.
+
+Ex.: 
+
+ENTRADA
+obj = new Object
+obj.nome = "Joao"
+obj.idade = 80
+obj.filhos = [ "Maria", "Pedro" ]
+
+SAIDA
+{
+	"nome": "Joao",
+	"idade": 80,
+	"filhos": [
+		"Maria",
+		"Pedro"
+	]
+}
+
+end
