d4d1916798f8ee0bf5f835460b92fd262d7e1f07
Jessé Cirilo Soares <jessecsoares@gmail.com>
2013-09-02 13:47:43 -0300

    Resolucao do exericio 1.1

--- exercicios/ex1_charactercounter.rb
+++ exercicios/ex1_charactercounter.rb
@@ -4,4 +4,13 @@
 
 2) Criar um método que receba 2 parâmetros, 1 texto e 1 array de caracteres e retorne a quantidade de vezes que os caracteres são encontrados no texto
 
+=end
+
+def char_count text, char
+	(text.count char).to_s + " ocorrência(s) do caracter '#{char}' encontrada(s)"
 end
+
+puts 'Digite o caracter que deseja buscar: '
+char = gets.chomp
+
+puts char_count 'Garçom, aqui nessa mesa de bar, voce já cansou de escutar, centenas de casos de amor...', char
