a8b01944c057087ca372732ef1746a2c407dda2e
kaduChessi <carlos.melo@lemaf.ufla.br>
2014-02-25 17:33:36 -0300

    Quase tudo implementado

--- exercicios/ex2_jsonparser.rb
+++ exercicios/ex2_jsonparser.rb
@@ -32,22 +32,45 @@ class Pessoa
 end	
 
 def toJson obj
+
 	json = "" 
+
+	if obj.kind_of?(Array)
+		obj.each do | value |
+
+			json += ", \n" if json.length > 0
+
+			if value.kind_of?(NilClass)
+				json += "null"
+			else	
+				json += "'" + value + "'"
+			end
+		end
+	end
+
 	obj.instance_variables.each do | key |
 		
+		prefixoJson = key.delete "@"
+
 		json += ", \n" if json.length > 0
 
 		if obj.instance_variable_get(key).kind_of?(Array)
-			json += key + " : " +  obj.instance_variable_get(key).to_s	
+			array = toJson obj.instance_variable_get(key)
+			json += prefixoJson + " : " +  array.to_s
 		elsif obj.instance_variable_get(key).kind_of?(String)
-			json += key + " : '" +  obj.instance_variable_get(key) + "'"
+			json += prefixoJson + " : '" +  obj.instance_variable_get(key) + "'"
 		else
-			json += key + " : " +  obj.instance_variable_get(key).to_s
-		end		
+			json += prefixoJson + " : " +  obj.instance_variable_get(key).to_s
+		end	
 	end	
-	puts "{\n" + json + "\n}" 
+
+	if obj.kind_of?(Array)
+		"[\n" + json + "\n]" 
+	else	
+		"{\n" + json + "\n}" 	
+	end	
 end	
 
 pessoa = Pessoa.new "Joao", 80, [ "Maria", "Pedro" ]
 
-toJson pessoa
+puts toJson pessoa
