2f959acff90048e33d3b68cd1aa318a1527e39dd
Jessé Cirilo Soares <jesse.soares@lemaf.ufla.br>
2014-02-26 15:05:41 -0300

    Implementacao do execicio 3

--- exercicios/ex3_array_numeric.rb
+++ exercicios/ex3_array_numeric.rb
@@ -13,3 +13,49 @@ Criar 3 métodos na classe Array:
 	Ex: [1, 'a', Pessoa.new, "4.4"].to_num!						# saida: [1, 4.4]
 
 =end
+
+class Array
+
+	def each_num &block
+
+		self.each { |x| block.call to_numeric x if to_numeric x }
+	end
+
+	def to_num
+
+		new_arr = []
+
+		self.each { |x| new_arr << (to_numeric x) if to_numeric x }
+
+		new_arr
+	end
+
+	def to_num!
+		self.replace to_num
+	end
+
+	private
+
+	def to_numeric value
+
+		return value if value.is_a? Numeric
+
+		return value.to_f if (value.respond_to? 'to_f') && (value.to_f != 0)
+
+		return value.to_i if (value.respond_to? 'to_i') && (value.to_i != 0)
+
+		false
+	end
+
+end
+
+# Teste
+arr1 = [1, 'a', Exception.new, "4.4", '1031']
+
+arr1.each_num { |x| puts x }
+
+puts arr1.to_num.inspect
+puts arr1.inspect
+
+puts arr1.to_num!.inspect
+puts arr1.inspect
