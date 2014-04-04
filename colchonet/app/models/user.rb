7cf3cb5c36e16ad171b09aec5b71681f070f9219
kaduChessi <carlos.melo@lemaf.ufla.br>
2014-04-04 10:35:29 -0300

    Criado controller de usuário, agora falta fazer os serviços na mão

--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+
+	validates_presence_of :email, :full_name, :location, :password
+	validates_confirmation_of :password
+	validates_length_of :bio, :minimum => 30, :allow_blanck => false
+	validates_format_of :email => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
 end
